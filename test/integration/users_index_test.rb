1a34ee45de2c7a1bd242e2824eef252eaec00b39
tp9106 <tp9106@gmail.com>
2015-11-24 09:41:53 +0000

    Fix error

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -8,7 +8,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   end
   
   test "index as admin including pagination and delete links" do
-    log_in_as(@user)
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
