e347afb31ccfacc85cc3b74adc02c786255733e7
tp9106 <tp9106@gmail.com>
2015-11-26 14:04:55 +0000

    fix host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'floating-hollows-2813.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.sendgrid.net',
     :port                 => '587',
